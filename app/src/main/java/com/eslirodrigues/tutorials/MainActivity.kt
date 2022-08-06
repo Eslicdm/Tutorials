94d2b663acce468fc4274b0c38bfec1b968452e8
Eslicdm <eslicdm1@gmail.com>
2022-08-06 10:02:05 -0300

    StickyHeader with LazyColumn Jetpack Compose

--- app/src/main/java/com/eslirodrigues/tutorials/MainActivity.kt
+++ app/src/main/java/com/eslirodrigues/tutorials/MainActivity.kt
@@ -3,7 +3,7 @@ package com.eslirodrigues.tutorials
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.eslirodrigues.tutorials.searchbar_state_viewmodel.ui.screen.SearchBarScreen
+import com.eslirodrigues.tutorials.stickyheader_lazycolumn.ui.screen.StickyHeaderScreen
 import com.eslirodrigues.tutorials.ui.theme.TutorialsTheme
 
 class MainActivity : ComponentActivity() {
@@ -11,7 +11,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             TutorialsTheme {
-                SearchBarScreen()
+                StickyHeaderScreen()
             }
         }
     }
---
+++ app/src/main/java/com/eslirodrigues/tutorials/stickyheader_lazycolumn/data/model/NameAge.kt
@@ -0,0 +1,6 @@
+package com.eslirodrigues.tutorials.stickyheader_lazycolumn.data.model
+
+data class NameAge(
+    val name: String,
+    val age: String
+)
