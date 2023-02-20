fcaa08cd7dde66e138bf0f850c0f2a857dc19f7b
Eslicdm <eslicdm1@gmail.com>
2023-02-20 15:57:53 -0300

    1# BottomAppBar

--- app/src/main/java/com/eslirodrigues/tutorials/bottom_app_bar/ui/screen/TutorialBottomAppBarScreen.kt
+++ app/src/main/java/com/eslirodrigues/tutorials/bottom_app_bar/ui/screen/TutorialBottomAppBarScreen.kt
@@ -1,5 +1,6 @@
 package com.eslirodrigues.tutorials.bottom_app_bar.ui.screen
 
+import android.annotation.SuppressLint
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Mic
@@ -9,13 +10,14 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.dp
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TutorialBottomAppBarScreen() {
     Scaffold(
         bottomBar = {
             BottomAppBar(
-                icons = {
+                actions = {
                     IconButton(onClick = { /*TODO*/ }) {
                         Icon(Icons.Default.Search, contentDescription = "Search")
                     }
@@ -33,9 +35,9 @@ fun TutorialBottomAppBarScreen() {
                     ) {
                         Icon(Icons.Default.Add, contentDescription = "Add")
                     }
-                },
+                }
             )
-        },
+        }
     ) {
 
     }
