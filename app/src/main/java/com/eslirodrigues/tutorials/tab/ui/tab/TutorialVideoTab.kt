809825529429aaae0c8554e194f4867e5646a86e
Eslicdm <eslicdm1@gmail.com>
2023-03-01 09:18:11 -0300

    Tab

---
+++ app/src/main/java/com/eslirodrigues/tutorials/tab/ui/tab/TutorialVideoTab.kt
@@ -0,0 +1,23 @@
+package com.eslirodrigues.tutorials.tab.ui.tab
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun TutorialVideoTab() {
+    Text(text = "Video", fontSize = 32.sp)
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Video", fontSize = 32.sp)
+    }
+}
