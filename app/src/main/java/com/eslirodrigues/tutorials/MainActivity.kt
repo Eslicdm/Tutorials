dc9578a01e70a26edabf3a5316873d69cdb89cc0
Eslicdm <eslicdm1@gmail.com>
2023-07-08 10:47:03 -0300

    Brush Gradient

--- app/src/main/java/com/eslirodrigues/tutorials/MainActivity.kt
+++ app/src/main/java/com/eslirodrigues/tutorials/MainActivity.kt
@@ -3,15 +3,8 @@ package com.eslirodrigues.tutorials
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.eslirodrigues.tutorials.bottom_sheet.ui.screen.TutorialBottomSheetScreen
-import com.eslirodrigues.tutorials.card.ui.screen.TutorialCardScreen
-import com.eslirodrigues.tutorials.change_theme.ui.screen.ChangeThemeScreen
-import com.eslirodrigues.tutorials.horizontal_pager_on_boarding.ui.screen.TutorialHorizontalPagerOnBoardingScreen
-import com.eslirodrigues.tutorials.network_state.ui.screen.TutorialNetworkStateScreen
-import com.eslirodrigues.tutorials.progress_indicator.ui.screen.TutorialProgressIndicatorScreen
-import com.eslirodrigues.tutorials.swipe_to_dismiss.ui.screen.TutorialSwipeToDismissScreen
-import com.eslirodrigues.tutorials.text_field.ui.screen.TutorialTextFieldScreen
-import com.eslirodrigues.tutorials.tooltip.ui.screen.TutorialToolTipScreen
+import com.eslirodrigues.tutorials.brush_gradient.ui.screen.TutorialBrushGradient
+import com.eslirodrigues.tutorials.canvas.ui.screen.TutorialCanvas
 import com.eslirodrigues.tutorials.ui.theme.TutorialsTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -22,7 +15,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             TutorialsTheme {
-                TutorialSwipeToDismissScreen()
+                TutorialBrushGradient()
             }
         }
     }
