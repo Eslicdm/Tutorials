61f8a0e295d7996463067f2921e27c631eefa3c7
Eslicdm <eslicdm1@gmail.com>
2023-10-24 15:09:04 -0300

    Drag Gesture

--- app/src/main/java/com/eslirodrigues/tutorials/MainActivity.kt
+++ app/src/main/java/com/eslirodrigues/tutorials/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.eslirodrigues.tutorials.animations.ui.screen.TutorialAnimationsScreen
 import com.eslirodrigues.tutorials.backpress.ui.screen.TutorialBackPressScreen
+import com.eslirodrigues.tutorials.drag.ui.screen.TutorialDragScreen
 import com.eslirodrigues.tutorials.ui.theme.TutorialsTheme
 import com.eslirodrigues.tutorials.window_manager.ui.navigation.TutorialWindowManagerNavGraph
 import dagger.hilt.android.AndroidEntryPoint
@@ -16,7 +17,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             TutorialsTheme {
-                TutorialBackPressScreen(this)
+                TutorialDragScreen()
             }
         }
     }
