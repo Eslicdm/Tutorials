5b619179df12e5c2ced2b7a0699d801fc732ad7a
Eslicdm <eslicdm1@gmail.com>
2023-10-24 14:59:06 -0300

    Animation Test

---
+++ app/src/androidTest/java/com/eslirodrigues/tutorials/animations/ui/screen/TutorialAnimationsTest.kt
@@ -0,0 +1,27 @@
+package com.eslirodrigues.tutorials.animations.ui.screen
+
+import androidx.compose.ui.test.assertTextEquals
+import androidx.compose.ui.test.hasClickAction
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.performClick
+import org.junit.Rule
+import org.junit.Test
+
+class TutorialAnimationsTest {
+    @get:Rule
+    val composeRule = createComposeRule()
+
+    @Test
+    fun assertTheAnimationValueIsEqual() {
+        composeRule.mainClock.autoAdvance = false
+
+        composeRule.setContent {
+            TutorialAnimatedContent()
+        }
+        composeRule.onNode(hasClickAction()).performClick()
+
+        composeRule.mainClock.advanceTimeBy(1000L)
+
+        composeRule.onNode(hasClickAction()).assertTextEquals("1")
+    }
+}
