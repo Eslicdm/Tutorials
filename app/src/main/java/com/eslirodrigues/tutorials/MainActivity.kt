8b26fe6fe533e809643e653e762ff5d467d80d03
Eslicdm <eslicdm1@gmail.com>
2024-04-10 09:16:46 -0300

    Swipe to Reveal

--- app/src/main/java/com/eslirodrigues/tutorials/MainActivity.kt
+++ app/src/main/java/com/eslirodrigues/tutorials/MainActivity.kt
@@ -3,7 +3,7 @@ package com.eslirodrigues.tutorials
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.eslirodrigues.tutorials.rotation.ui.screen.TutorialRotationScreen
+import com.eslirodrigues.tutorials.swipe_to_reveal.ui.screen.TutorialSwipeToRevealScreen
 import com.eslirodrigues.tutorials.ui.theme.TutorialsTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -15,7 +15,7 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             TutorialsTheme {
-                TutorialRotationScreen()
+                TutorialSwipeToRevealScreen()
             }
         }
     }
