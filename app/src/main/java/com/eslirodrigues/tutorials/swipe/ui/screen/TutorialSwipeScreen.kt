dec048e553516f0cec78f8999bd962439392cc42
Eslicdm <eslicdm1@gmail.com>
2024-04-10 08:38:03 -0300

    #2 Swipe Animation

--- app/src/main/java/com/eslirodrigues/tutorials/swipe/ui/screen/TutorialSwipeScreen.kt
+++ app/src/main/java/com/eslirodrigues/tutorials/swipe/ui/screen/TutorialSwipeScreen.kt
@@ -7,53 +7,89 @@ import androidx.compose.foundation.gestures.AnchoredDraggableState
 import androidx.compose.foundation.gestures.DraggableAnchors
 import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.anchoredDraggable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import kotlin.math.roundToInt
 
-enum class DragValue { Start, Center, End }
+private enum class VerticalDragValue { Settled, TopToBottom, BottomToTop }
+private enum class HorizontalDragValue { Settled, StartToEnd, EndToStart }
+
+@Composable
+fun TutorialSwipeScreen() {
+    TutorialDynamicSwipe()
+    TutorialStaticSwipe()
+}
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun TutorialSwipeScreen() {
-    val density = LocalDensity.current
-    val screenSizeDp = LocalConfiguration.current.screenWidthDp.dp
-    val screenSizePx = with(density) { screenSizeDp.toPx() }
-    val anchors = remember {
-        DraggableAnchors {
-            DragValue.Start at 0f
-            DragValue.Center at screenSizePx / 2
-            DragValue.End at screenSizePx
-        }
-    }
+fun TutorialDynamicSwipe() {
+    var boxWidth by remember { mutableFloatStateOf(0f) }
     val state = remember {
         AnchoredDraggableState(
-            initialValue = DragValue.Start,
+            initialValue = HorizontalDragValue.Settled,
             positionalThreshold = { distance -> distance * 0.3f },
-            velocityThreshold = {  with(density) { 100.dp.toPx() }  },
+            velocityThreshold = { 0.3f },
             animationSpec = tween(),
         )
+    }
+    val anchors = DraggableAnchors {
+        HorizontalDragValue.Settled at 0f
+        HorizontalDragValue.StartToEnd at boxWidth / 3
+        HorizontalDragValue.EndToStart at -boxWidth / 3
     }
     SideEffect { state.updateAnchors(anchors) }
     Box(
         Modifier
-            .fillMaxSize()
-            .anchoredDraggable(state, Orientation.Vertical)
+            .graphicsLayer { boxWidth = size.width }
+            .offset { IntOffset(x = state.requireOffset().roundToInt(), y = 0) }
+            .height(400.dp)
+            .width(200.dp)
+            .anchoredDraggable(state, Orientation.Horizontal)
             .background(Color.LightGray)
-    ) {
-        Box(
-            Modifier
-                .offset { IntOffset(x = 0, y = state.requireOffset().roundToInt()) } // x = 0 Horizontal
-                .size(screenSizeDp)
-                .background(Color.DarkGray)
+    )
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun TutorialStaticSwipe() {
+    val density = LocalDensity.current
+    val screenSizeDp = LocalConfiguration.current.screenWidthDp.dp
+    val screenSizePx = with(density) { screenSizeDp.toPx() }
+    val anchors = DraggableAnchors {
+        VerticalDragValue.Settled at 0f
+        VerticalDragValue.TopToBottom at screenSizePx / 2
+        VerticalDragValue.BottomToTop at -screenSizePx / 2
+    }
+    val state = remember {
+        AnchoredDraggableState(
+            initialValue = VerticalDragValue.Settled,
+            anchors = anchors,
+            positionalThreshold = { distance -> distance * 0.3f },
+            velocityThreshold = { 0.3f },
+            animationSpec = tween(),
         )
     }
+    Box(
+        Modifier
+            .offset { IntOffset(x = 0, y = state.requireOffset().roundToInt()) }
+            .size(screenSizeDp)
+            .anchoredDraggable(state, Orientation.Vertical)
+            .background(Color.DarkGray)
+    )
 }
