5525fc2e78f279facc17c689024edde136a551fc
Eslicdm <eslicdm1@gmail.com>
2024-04-10 08:57:58 -0300

    Lifecycle

--- app/src/main/java/com/eslirodrigues/tutorials/MainActivity.kt
+++ app/src/main/java/com/eslirodrigues/tutorials/MainActivity.kt
@@ -3,7 +3,7 @@ package com.eslirodrigues.tutorials
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.eslirodrigues.tutorials.translate.ui.screen.TutorialTranslateScreen
+import com.eslirodrigues.tutorials.lifecycle.ui.screen.TutorialLifecycleScreen
 import com.eslirodrigues.tutorials.ui.theme.TutorialsTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -15,7 +15,7 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             TutorialsTheme {
-                TutorialTranslateScreen()
+                TutorialLifecycleScreen()
             }
         }
     }
