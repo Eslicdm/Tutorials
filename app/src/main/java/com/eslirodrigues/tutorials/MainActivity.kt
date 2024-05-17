815b267eaf06b0cd900a30834504190364c21026
Eslicdm <eslicdm1@gmail.com>
2024-05-17 09:01:24 -0300

    #1 Zoom

--- app/src/main/java/com/eslirodrigues/tutorials/MainActivity.kt
+++ app/src/main/java/com/eslirodrigues/tutorials/MainActivity.kt
@@ -3,8 +3,8 @@ package com.eslirodrigues.tutorials
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.eslirodrigues.tutorials.input_mask.ui.screen.TutorialInputMaskScreen
 import com.eslirodrigues.tutorials.ui.theme.TutorialsTheme
+import com.eslirodrigues.tutorials.zoom.ui.screen.TutorialZoomScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -12,10 +12,9 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContent {
             TutorialsTheme {
-                TutorialInputMaskScreen()
+                TutorialZoomScreen()
             }
         }
     }
