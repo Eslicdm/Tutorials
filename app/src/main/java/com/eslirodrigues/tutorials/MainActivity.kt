4fbdd2ec21cf02d6bc4bf2d238352b7f7ce2ef1c
Eslicdm <eslicdm1@gmail.com>
2024-05-21 09:24:20 -0300

    #1 Rotation

--- app/src/main/java/com/eslirodrigues/tutorials/MainActivity.kt
+++ app/src/main/java/com/eslirodrigues/tutorials/MainActivity.kt
@@ -3,7 +3,7 @@ package com.eslirodrigues.tutorials
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.eslirodrigues.tutorials.navigation.ui.navigation.TutorialNavGraph
+import com.eslirodrigues.tutorials.rotation.ui.screen.TutorialRotationScreen
 import com.eslirodrigues.tutorials.ui.theme.TutorialsTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -14,7 +14,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             TutorialsTheme {
-                TutorialNavGraph()
+                TutorialRotationScreen()
             }
         }
     }
