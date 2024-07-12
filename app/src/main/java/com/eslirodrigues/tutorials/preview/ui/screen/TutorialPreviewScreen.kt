7f7ff1f3cda940903104f06de8a0df8e1911ab1b
Eslicdm <eslicdm1@gmail.com>
2024-07-12 08:06:19 -0300

    Preview Ui Check Mode

--- app/src/main/java/com/eslirodrigues/tutorials/preview/ui/screen/TutorialPreviewScreen.kt
+++ app/src/main/java/com/eslirodrigues/tutorials/preview/ui/screen/TutorialPreviewScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import com.eslirodrigues.tutorials.preview.ui.preview.TutorialFoldablePreview
 import com.eslirodrigues.tutorials.preview.ui.preview.TutorialPhonePreview
@@ -30,7 +31,8 @@ fun TutorialPreviewScreen(stateValues: Int = 0) {
                     0 -> "First State"
                     1 -> "Second State"
                     else -> "Else State"
-                }
+                },
+                color = Color.Blue // Ui Check Mode Error
             )
         }
     }
